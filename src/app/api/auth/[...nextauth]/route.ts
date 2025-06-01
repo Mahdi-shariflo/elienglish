10ab4315b5233a85ea5269736a17638b2410c56c
mahdi-shariflo <78957548+Mahdi-shariflo@users.noreply.github.com>
2025-06-01 08:42:21 +0330

    test:auth google

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,10 +5,26 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_SECRET_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET_ID as string,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }: any) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  // Optional: Configure JWT to store the access token
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 export const handler = NextAuth(authOptions);
