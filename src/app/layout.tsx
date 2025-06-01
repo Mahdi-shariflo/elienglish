4de940556eb759366bcdbf4694d93f3f38f6fc76
mahdi-shariflo <78957548+Mahdi-shariflo@users.noreply.github.com>
2025-06-01 08:20:21 +0330

    test:auth google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import './globals.css';
 import Fetcher from '@/components/common/Fetcher';
 import GlobalContextProvider from '@/components/common/GlobalContextProvider';
 import { SessionProvider } from '@/lib/auth/SessionProvider';
+import { SessionProvider as SessionProviderNextAuth } from 'next-auth/react';
+
 import { Viewport } from 'next';
+import { getSession } from 'next-auth/react';
 export const viewport: Viewport = {
   themeColor: '#DD338B',
   colorScheme: 'only light',
@@ -17,16 +20,18 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const session = await getSession();
+  const session = await getSession();
 
   return (
     <html lang="fa">
       <body>
-        <SessionProvider session={null}>
-          <Fetcher>
-            <GlobalContextProvider>{children}</GlobalContextProvider>
-          </Fetcher>
-        </SessionProvider>
+        <SessionProviderNextAuth session={session}>
+          <SessionProvider session={null}>
+            <Fetcher>
+              <GlobalContextProvider>{children}</GlobalContextProvider>
+            </Fetcher>
+          </SessionProvider>
+        </SessionProviderNextAuth>
       </body>
     </html>
   );
