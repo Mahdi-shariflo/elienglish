2a939595e239a4d435c95fda5991826d1c88d3ac
mahdi-shariflo <78957548+Mahdi-shariflo@users.noreply.github.com>
2025-06-11 14:44:26 +0330

    fix:fix respansive blog page

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -5,14 +5,18 @@ import Image from 'next/image';
 const Logo = ({ className }: { className?: string }) => {
   return (
     <>
-      <Link className={'flex items-center gap-2'} href="/">
-        <Image className="h-[56px] w-[56px] rounded-full" src={LogoImage} alt="" />
+      <Link className={`$flex items-center gap-2 ${className}`} href="/">
+        <Image
+          className="h-[45px] w-[45px] rounded-full lg:h-[56px] lg:w-[56px]"
+          src={LogoImage}
+          alt=""
+        />
         <div>
           <div>
-            <span className="inline-block font-bold dark:text-white">آکادمی</span>
-            <span className="inline-block font-bold dark:text-main">آلی انگلیش</span>
+            <span className="inline-block font-bold text-[14px] dark:text-white">آکادمی</span>
+            <span className="inline-block font-bold text-[14px] dark:text-main">آلی انگلیش</span>
           </div>
-          <span className="dark:text-[#E5EAEF]">Elienglish Academy</span>
+          <span className="text-[14px] dark:text-[#E5EAEF]">Elienglish Academy</span>
         </div>
       </Link>
     </>
