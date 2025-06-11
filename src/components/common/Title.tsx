5288be2646278cb3f547cfa0e9a70eded838d225
mahdi-shariflo <78957548+Mahdi-shariflo@users.noreply.github.com>
2025-06-11 14:34:48 +0330

    fix:fix respansive blog page

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({ title }: { title: string }) => {
   return (
-    <p className="relative pr-4 font-extrabold text-[14px] after:absolute after:right-0 after:h-full after:w-1 after:rounded-bl-full after:rounded-tl-full after:bg-main dark:text-white lg:text-2xl">
+    <p className="relative pr-4 font-extrabold text-[16px] after:absolute after:right-0 after:h-full after:w-1 after:rounded-bl-full after:rounded-tl-full after:bg-main dark:text-white lg:text-2xl">
       {title}
     </p>
   );
