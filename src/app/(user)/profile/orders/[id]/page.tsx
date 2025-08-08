72de16bbc98556dd57b568afa7b32b1d844b14b9
mahdi-shariflo <78957548+Mahdi-shariflo@users.noreply.github.com>
2025-08-07 23:48:03 -0700

    fix:style sidebar

--- src/app/(user)/profile/orders/[id]/page.tsx
+++ src/app/(user)/profile/orders/[id]/page.tsx
@@ -7,6 +7,8 @@ import { discountCalculation } from '@/lib/utils';
 import BackPrevPage from '@/components/common/BackPrevPage';
 import { safeRequest } from '@/lib/safeClient';
 import { statusIcon } from '@/lib/data';
+import Link from 'next/link';
+import { GoArrowLeft } from 'react-icons/go';
 type Props = {
   params: Promise<{
     id: string;
@@ -17,12 +19,16 @@ const Page = async ({ params }: Props) => {
   const data = await safeRequest({ url: `/order/product-physical/${id}` });
   const order: Order = data?.data?.data;
   return (
-    <div className="space-y-4 rounded-2xl border-[#E4E7E9] bg-white pt-4 lg:mb-10 lg:mt-5 lg:!w-full lg:border lg:p-[16px] lg:pt-0">
+    <div className="space-y-4 rounded-2xl border-[#E4E7E9] bg-white pt-4 dark:border-[#263248] dark:bg-[#172334] lg:mb-10 lg:mt-5 lg:!w-full lg:border lg:p-[16px] lg:pt-0">
       <BackPrevPage url="/profile/orders" title="همه سفارشات" />
-
-      <p className="container_page font-medium text-[18px] text-[#0C0C0C] lg:w-full">
-        جزئیات سفارش
-      </p>
+      <div className="flex items-center justify-between">
+        <p className="container_page font-medium text-[18px] text-[#0C0C0C] dark:text-white lg:w-full">
+          جزئیات سفارش
+        </p>
+        <Link href={'/profile/orders'}>
+          <GoArrowLeft className="text-white" size={24} />
+        </Link>
+      </div>
       <div className="mt-5 px-5 lg:!mt-10">
         {/* title */}
         {/* <div className='flex items-center justify-between '>
@@ -31,15 +37,15 @@ const Page = async ({ params }: Props) => {
                         مشاهده فاکتور
                     </Button>
                 </div> */}
-        <div className="flex h-[56px] items-center bg-[#F5F6F6] px-4">
+        <div className="flex h-[56px] items-center bg-[#F5F6F6] px-4 dark:bg-gray-800">
           <p className="w-[240px] font-regular text-[14px] text-[#7D8793]">کد پیگیری سفارش</p>
-          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C]">
+          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C] dark:text-white">
             {order.orderNumber}
           </p>
         </div>
-        <div className="flex h-[56px] items-center bg-white px-4">
+        <div className="flex h-[56px] items-center bg-white px-4 dark:bg-gray-600">
           <p className="w-[240px] font-regular text-[14px] text-[#7D8793]">تاریخ ثبت سفارش</p>
-          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C]">
+          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C] dark:text-white">
             {new Date(order.createdAt).toLocaleDateString('fa-IR', {
               year: 'numeric',
               month: 'long',
@@ -47,46 +53,48 @@ const Page = async ({ params }: Props) => {
             })}
           </p>
         </div>
-        <div className="flex h-[56px] items-center bg-[#F5F6F6] px-4">
+        <div className="flex h-[56px] items-center bg-[#F5F6F6] px-4 dark:bg-gray-800">
           <p className="w-[240px] font-regular text-[14px] text-[#7D8793]">تحویل گیرنده</p>
-          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C]">
+          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C] dark:text-white">
             {order?.productPhysicalItems?.orderAddress?.firstName}{' '}
             {order?.productPhysicalItems?.orderAddress?.lastName}
           </p>
         </div>
-        <div className="flex h-[56px] items-center bg-white px-4">
+        <div className="flex h-[56px] items-center bg-white px-4 dark:bg-gray-600">
           <p className="w-[240px] font-regular text-[14px] text-[#7D8793]">شماره موبایل</p>
-          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C]">
+          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C] dark:text-white">
             {order?.productPhysicalItems?.orderAddress?.mobileNumber}
           </p>
         </div>
-        <div className="flex min-h-[56px] items-center bg-[#F5F6F6] px-4 py-4">
+        <div className="flex min-h-[56px] items-center bg-[#F5F6F6] px-4 py-4 dark:bg-gray-800">
           <p className="w-[240px] font-regular text-[14px] text-[#7D8793]">آدرس</p>
-          <p className="font-regular text-[14px] text-[#0C0C0C]">
+          <p className="font-regular text-[14px] text-[#0C0C0C] dark:text-white">
             {order?.productPhysicalItems?.orderAddress?.address}
           </p>
         </div>
       </div>
 
-      <div className="h-px w-full bg-[#E4E7E9]" />
+      <div className="h-px w-full bg-[#E4E7E9] dark:bg-[#263248]" />
 
       <div className="!mt-10 px-5">
         {/* title */}
-        <p className="mb-3 font-regular text-[#393B40]">جزئیات پرداخت و مرسوله </p>
-        <div className="flex h-[56px] items-center bg-[#F5F6F6] px-4">
+        <p className="mb-3 font-regular text-[#393B40] dark:text-white">جزئیات پرداخت و مرسوله </p>
+        <div className="flex h-[56px] items-center bg-[#F5F6F6] px-4 dark:bg-gray-800">
           <p className="w-[240px] font-regular text-[14px] text-[#7D8793]">مبلغ سفارش</p>
-          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C]">
+          <p className="whitespace-nowrap font-regular text-[14px] text-[#0C0C0C] dark:text-white">
             {Number(order?.totalAmount).toLocaleString()} تومان
           </p>
         </div>
 
-        <div className="flex h-[56px] items-center bg-[#F5F6F6] px-4">
-          <p className="w-[240px] font-regular text-[14px] text-[#7D8793]">وضعیت </p>
+        <div className="flex h-[56px] items-center bg-[#F5F6F6] px-4 dark:bg-gray-800">
+          <p className="w-[240px] font-regular text-[14px] text-[#7D8793] dark:text-white">
+            وضعیت{' '}
+          </p>
           <div className="flex items-center gap-2 lg:p-[16px]">
             <span>
               {statusIcon.find((item) => item.status === order.productPhysicalItems.status)?.icon}
             </span>
-            <span className="font-regular text-[14px] text-[#393B40]">
+            <span className="font-regular text-[14px] text-[#393B40] dark:text-white">
               {statusIcon.find((item) => item.status === order.productPhysicalItems.status)?.name}
             </span>
           </div>
@@ -98,16 +106,19 @@ const Page = async ({ params }: Props) => {
                             <path fillRule="evenodd" clipRule="evenodd" d="M7.81542 10.5716C7.69542 10.6807 7.54459 10.7349 7.39459 10.7349C7.22459 10.7349 7.05542 10.6657 6.93209 10.5307L5.41292 8.86408C5.18042 8.60908 5.19876 8.21323 5.45376 7.98156C5.70792 7.7474 6.10376 7.76656 6.33709 8.02155L7.85626 9.68825C8.08876 9.94408 8.07042 10.3391 7.81542 10.5716ZM17.5596 7.35073L15.1087 3.75156C14.7279 3.18823 14.0946 2.85156 13.4137 2.85156H6.59126C5.91126 2.85156 5.27792 3.18822 4.89792 3.74989L2.43876 7.35073C1.90459 8.13406 1.97709 9.17825 2.61626 9.89075L8.70458 16.5741C9.03542 16.9382 9.50708 17.1474 9.99875 17.1474C10.4904 17.1474 10.9621 16.9382 11.2921 16.5732L17.3812 9.88241C18.0229 9.17908 18.0962 8.13823 17.5596 7.35073Z" fill="#FFA216" />
                         </svg>
 
-                        <p className='text-[#0C0C0C] font-regular text-[14px] whitespace-nowrap'>۵۴</p>
+                        <p className='text-[#0C0C0C] dark:text-white font-regular text-[14px] whitespace-nowrap'>۵۴</p>
                     </div>
                 </div> */}
       </div>
 
-      <div className="h-px w-full bg-[#E4E7E9]" />
+      <div className="h-px w-full bg-[#E4E7E9] dark:bg-[#263248]" />
 
       <div className="container_page flex flex-col !gap-5 lg:w-full">
         {order.productPhysicalItems.products.map((product, idx) => (
-          <div key={idx} className="flex items-start gap-3 border-b border-[#E4E7E9] pb-3">
+          <div
+            key={idx}
+            className="flex items-start gap-3 border-b border-[#E4E7E9] pb-3 dark:border-[#263248]"
+          >
             <div>
               <span className="relative block h-[116px] w-[116px]">
                 <Image
@@ -124,7 +135,7 @@ const Page = async ({ params }: Props) => {
                 <p>x{product?.count}</p>
               </div>
               <div className="flex flex-col items-end">
-                <p className="flex items-center gap-1 font-regular text-[14px] text-[#0C0C0C]">
+                <p className="flex items-center gap-1 font-regular text-[14px] text-[#0C0C0C] dark:text-white">
                   {product.discountPrice
                     ? Number(product.discountPrice).toLocaleString()
                     : Number(product.price).toLocaleString()}
